f57cb567a725c6cf36cde6f2320b39ebc6baed59
CKiilu <cndeti@gmail.com>
2016-02-01 08:55:00 +0300

    Created .class file

---
+++ src/firsttoyp/Month.java
@@ -0,0 +1,15 @@
+/*Using Java, declare an integer named month whose value represents a month.
+ *  Your code should display the name of the month, 
+ *  based on the value of month, using the switch statement.
+*/
+
+package firsttoyp;
+
+public class Month {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
