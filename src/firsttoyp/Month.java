05c32848542285db659297810137e4e0b02c08e4
CKiilu <cndeti@gmail.com>
2016-02-01 09:27:24 +0300

    FInished product

--- src/firsttoyp/Month.java
+++ src/firsttoyp/Month.java
@@ -4,12 +4,55 @@
 */
 
 package firsttoyp;
+import java.util.*;
 
 public class Month {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Type out the month below, using numbers 0-11");
+		switch (sc.nextInt()) {
+		case 0:
+			System.out.println("January");
+			break;
+		case 1:
+			System.out.println("February");
+			break;
+		case 2:
+			System.out.println("March");
+			break;
+		case 3:
+			System.out.println("April");
+			break;
+		case 4:
+			System.out.println("May");
+			break;
+		case 5:
+			System.out.println("June");
+			break;
+		case 6:
+			System.out.println("July");
+			break;
+		case 7:
+			System.out.println("August");
+			break;
+		case 8:
+			System.out.println("September");
+			break;
+		case 9:
+			System.out.println("October");
+			break;
+		case 10:
+			System.out.println("November");
+			break;
+		case 11:
+			System.out.println("December");
+			break;
+		default:
+			System.out.println("Not a  month");
+			break;
+		}
 	}
 
 }
